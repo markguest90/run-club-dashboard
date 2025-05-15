2112a29c233ee938eef9d971a24016fd9f6e6225
markguest90 <markguest90@gmail.com>
2025-05-15 23:19:03 +0100

    Update running_club_dashboard.py

--- running_club_dashboard.py
+++ running_club_dashboard.py
@@ -418,7 +418,7 @@ heat_data = location_counts[['lat', 'lon', 'weight']].values.tolist()
 
 location_map = folium.Map(location=[53.38, -3.07], zoom_start=10)
 HeatMap(heat_data).add_to(location_map)
-components.html(location_map._repr_html_(), height=350)
+components.html(location_map._repr_html_(), height=500)
 
 st.subheader("🏅 Most Frequent Attenders")
 filtered = exploded['Runner'].value_counts().reset_index()
