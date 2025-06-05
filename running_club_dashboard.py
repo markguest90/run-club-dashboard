5beb7d409440354f92540b62d14639b3e8065fbe
markguest90 <markguest90@gmail.com>
2025-06-05 13:28:26 +0100

    Update running_club_dashboard.py

--- running_club_dashboard.py
+++ running_club_dashboard.py
@@ -116,12 +116,12 @@ exploded['Runner'] = exploded['RunnerList'].str.strip()
 # ------------------------
 # Dashboard Title - 4 variants
 # ------------------------
-st.markdown("""
-<div style='text-align: center;'>
-    <h1 style='font-size: 2.8em;'>Run Club 🏃‍♂️Dashboard🏃‍♀️</h1>
-    <p style='font-size: 1.2em; color: gray;'>Celebrate your achievements, track your streaks, and explore your run club stats!</p>
-</div>
-""", unsafe_allow_html=True)
+#st.markdown("""
+#<div style='text-align: center;'>
+#    <h1 style='font-size: 2.8em;'>Run Club 🏃‍♂️Dashboard🏃‍♀️</h1>
+#    <p style='font-size: 1.2em; color: gray;'>Celebrate your achievements, track your streaks, and explore your run club stats!</p>
+#</div>
+#""", unsafe_allow_html=True)
 
 # ---------------------
 # Refresh option
@@ -135,13 +135,13 @@ with col3:
         st.cache_data.clear()
         st.rerun()
 
-#st.markdown(
-#    """
-#    <div style='text-align: center; padding: 1rem; background-color: #f0f8ff; border-radius: 10px;'>
-#        <h1 style='margin-bottom: 0.5rem;'>Arrowe Park ED Run Club 🏃‍♀️Dashboard🏃‍♀️</h1>
-#    </div>
-#    """,
-#    unsafe_allow_html=True)
+st.markdown(
+    """
+    <div style='text-align: center; padding: 1rem; background-color: #f0f8ff; border-radius: 10px;'>
+        <h1 style='margin-bottom: 0.5rem;'>Run Club 🏃‍♂️Dashboard🏃‍♀️</h1>
+    </div>
+    """,
+    unsafe_allow_html=True)
 
 #st.markdown("""
 # <div style='text-align: center; background-color: #f0f8ff; padding: 1em; border-radius: 12px;'>
