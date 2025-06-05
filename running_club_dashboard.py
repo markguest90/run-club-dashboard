a5efba7803389e071f9cf48e749e1a5931089867
markguest90 <markguest90@gmail.com>
2025-06-05 13:25:19 +0100

    Update running_club_dashboard.py

--- running_club_dashboard.py
+++ running_club_dashboard.py
@@ -173,7 +173,7 @@ st.markdown(
         color: #333;
         text-shadow: 2px 2px 4px rgba(0,0,0,0.2);
     '>
-        Arrowe Park ED Run Club 🏃‍♂️Dashboard🏃‍♀️
+        ED Run Club 🏃‍♂️Dashboard🏃‍♀️
     </h1>
     """,
     unsafe_allow_html=True
